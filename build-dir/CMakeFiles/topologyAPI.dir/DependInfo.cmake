ecdead8e0de1b54a9ff72255b867ba1d445ee97a
AmmarYasser97 <ammaryasser085@gmail.com>
2021-12-12 14:21:29 +0200

    Build the API with a building tool (cmake)

---
+++ build-dir/CMakeFiles/topologyAPI.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ammar/Electronics-Topology-API/Memory.cpp" "/home/ammar/Electronics-Topology-API/build-dir/CMakeFiles/topologyAPI.dir/Memory.cpp.o"
+  "/home/ammar/Electronics-Topology-API/main.cpp" "/home/ammar/Electronics-Topology-API/build-dir/CMakeFiles/topologyAPI.dir/main.cpp.o"
+  "/home/ammar/Electronics-Topology-API/nmos.cpp" "/home/ammar/Electronics-Topology-API/build-dir/CMakeFiles/topologyAPI.dir/nmos.cpp.o"
+  "/home/ammar/Electronics-Topology-API/resistance.cpp" "/home/ammar/Electronics-Topology-API/build-dir/CMakeFiles/topologyAPI.dir/resistance.cpp.o"
+  "/home/ammar/Electronics-Topology-API/topology.cpp" "/home/ammar/Electronics-Topology-API/build-dir/CMakeFiles/topologyAPI.dir/topology.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-dir/CMakeFiles/topologyAPI.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/topologyAPI.dir/Memory.cpp.o"
+  "CMakeFiles/topologyAPI.dir/main.cpp.o"
+  "CMakeFiles/topologyAPI.dir/nmos.cpp.o"
+  "CMakeFiles/topologyAPI.dir/resistance.cpp.o"
+  "CMakeFiles/topologyAPI.dir/topology.cpp.o"
+  "topologyAPI"
+  "topologyAPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/topologyAPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
